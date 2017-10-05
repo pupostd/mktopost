feed4d64b243808d03e2911a2390eaad0177ee68
Pupo <pupo@tutanota.com>
2017-10-05 15:56:53 -0300

    Testing Atom lint and Go packages.

--- core/searcher.go
+++ core/searcher.go
@@ -1,20 +1,27 @@
 package core
 
+import "fmt"
+
+//Searcher A class to search and read the resource requested.
 type Searcher struct {
-  placesToSearch []string
+	placesToSearch []string
 }
 
+// SearchRequested searches for a requested file.
 func (s Searcher) SearchRequested(fileName string) string {
-  s.loadPlacesToSearch()
-  return fileName
+	s.loadPlacesToSearch()
+
+	fmt.Printf("Received name to search %s\n", fileName)
+
+	return fileName
 }
 
 func (s Searcher) addPlacesToSearch(path string) {
-  s.placesToSearch = append(s.placesToSearch, path)
+	s.placesToSearch = append(s.placesToSearch, path)
 }
 
 func (s Searcher) loadPlacesToSearch() {
-  //s.placesToSearch := [1]string{"searcher.properties"}
-  s.addPlacesToSearch("searcher.properties")
-  s.addPlacesToSearch("default.properties")
+	//s.placesToSearch := [1]string{"searcher.properties"}
+	s.addPlacesToSearch("to_publish")
+	s.addPlacesToSearch("to_publish_hidden")
 }
