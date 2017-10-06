a5f13e9fc6775bc34527b27ff9f8fe69e72c4e1f
pupostd <pupo@tutanota.com>
2017-10-06 16:39:07 -0300

    More tests.

--- core/configuration_test.go
+++ core/configuration_test.go
@@ -1,9 +1,36 @@
 package core
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestConfiguration(t *testing.T) {
 	conf := Configuration{}
 	conf.Load()
 
+	if len(conf.Properties) != 3 {
+		t.Error("Not enough properties on property file! Shoud be 3.")
+	}
+
+	var count int
+	for _, v := range conf.Properties {
+
+		if "to.parse" == v.Name {
+			if "../resource/to_parse/" == v.Value {
+				count++
+			}
+		} else if "to.publish" == v.Name {
+			if "../resource/to_publish/" == v.Value {
+				count++
+			}
+		} else if "to.publish.hidden" == v.Name {
+			if "../resource/to_publish_hidden/" == v.Value {
+				count++
+			}
+		}
+	}
+
+	if count != 3 {
+		t.Error("Wrong properties defined on property file!")
+	}
 }
